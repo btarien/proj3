0f616a08201b362426663075e36e0aaf714366f2
btarien <bmtarien@ucdavis.edu>
2016-02-02 20:07:54 -0800

    Update decoder.cpp

--- p3/decoder.cpp
+++ p3/decoder.cpp
@@ -11,12 +11,12 @@ using namespace std;
 
 void Decoder::addl(Decoder *decoder)
 {
-  *setOperand2(*getOperand1() + *getOperand2());
+  setOperand2(getOperand1() + getOperand2());
 }  // addl()
 
 void Decoder::andl(Decoder *decoder)
 {
-  *setOperand2(*getOperand1() & *getOperand2());
+  setOperand2(getOperand1() & getOperand2());
 }  // andl()
 
 void Decoder::execute(Decoder *decoder, Registers *registers, int memory[1001])
@@ -47,19 +47,19 @@ void Decoder::execute(Decoder *decoder, Registers *registers, int memory[1001])
 void Decoder::leave(Registers *registers, int memory[1001])
 {
   //registers->regs[esp] = registers->regs[ebp];
-  registers.set(esp, registers.get(ebp));
+  registers->set(esp, registers->get(ebp));
   
   //registers->regs[ebp] = memory[registers->regs[esp]];
-  registers.set(ebp, memory[registers.get(esp)]);
+  registers->set(ebp, memory[registers->get(esp)]);
   
   //registers->regs[esp] += 4;
-  registers.set(esp, registers.get(eip)+4);
+  registers->set(esp, registers->get(eip)+4);
 }  // leave()
 
 
 void Decoder::movl(Decoder *decoder)
 {
-  *setOperand2(*getOperand1);
+  setOperand2(getOperand1());
 }  // movl()
 
 
@@ -109,5 +109,5 @@ void Decoder::ret(Registers *registers, int memory[1001])
 
 void Decoder::subl(Decoder *decoder)
 {
-  *setOperand2(*getOperand2() - *getOperand1());
+  setOperand2(getOperand2() - getOperand1());
 }  // subl()
