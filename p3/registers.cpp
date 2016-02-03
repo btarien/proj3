6286b0ac3c4fdb7edf9698abbb4fe04cabe0fd68
btarien <bmtarien@ucdavis.edu>
2016-02-02 20:23:41 -0800

    Update registers.cpp

--- p3/registers.cpp
+++ p3/registers.cpp
@@ -5,18 +5,18 @@
 
 void initialize(Registers *registers, int memory[1001])
 {
-  registers.set(esp, 1000);//registers->regs[esp] = 1000;
-  registers.set(ebp, 996);//registers->regs[ebp] = 996;
-  registers.set(eip, 100);//registers->regs[eip] = 100;
-  registers.set(eax, 0);//registers->regs[eax] = 0;
+  registers->set(esp, 1000);//registers->regs[esp] = 1000;
+  registers->set(ebp, 996);//registers->regs[ebp] = 996;
+  registers->set(eip, 100);//registers->regs[eip] = 100;
+  registers->set(eax, 0);//registers->regs[eax] = 0;
   memory[1000] = 0;
 }  // initialize()
 
 void print(Registers *registers, const char *instruction)
 {
   printf("%-20s eip: %3d eax: %3d ebp: %3d esp: %3d\n", instruction,
-    registers.get(eip), registers.get(eax), registers.get(ebp),
-    registers.get(esp);
+    registers->get(eip), registers->get(eax), registers->get(ebp),
+    registers->get(esp);
 }  // print()
 
 int* address(Registers *registers, char *operand, int memory[])
