b4f6ff9c80b2c8e4e585f88419e4963cd853e435
btarien <bmtarien@ucdavis.edu>
2016-02-04 21:17:05 -0800

    Update instruction.cpp

--- p3/instruction.cpp
+++ p3/instruction.cpp
@@ -1,2 +1,42 @@
-// Author: Sean Davis
+#include <cstring>
 #include "instruction.h"
+
+
+Instruction::Instruction()
+{
+	info = 0;
+} // constructor
+
+
+Instruction::~Instruction()
+{
+	if(info != 0)
+		delete[] info;
+} // destructor
+
+
+const char* Instruction::getInfo()
+{
+	return info;
+} // getInfo()
+
+
+const int Instruction::getAddress()
+{
+	return address;
+} // getAddress()
+
+
+void Instruction::setInfo(const char* inf)
+{
+	int len=strlen(inf); 
+	info = new char[len + 1];
+
+	strcpy(info, inf);
+} // setInfo()
+
+
+void Instruction::setAddress(int addr)
+{
+	address = addr;
+} //setAddress()
