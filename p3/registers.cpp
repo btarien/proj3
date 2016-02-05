c8e8232b22fa2417371c6574c52d07f9377fd223
btarien <bmtarien@ucdavis.edu>
2016-02-04 21:15:24 -0800

    Update registers.cpp

--- p3/registers.cpp
+++ p3/registers.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <iomanip>
-#include <cstring>
 #include <cstdlib>
+#include <cstring>
 #include "registers.h"
 
 using namespace std;
 
-void initialize(Registers *registers, int memory[1001])
+void Registers::initialize(int memory[1001])
 {
-  registers->set(esp, 1000);//registers->regs[esp] = 1000;
-  registers->set(ebp, 996);//registers->regs[ebp] = 996;
-  registers->set(eip, 100);//registers->regs[eip] = 100;
-  registers->set(eax, 0);//registers->regs[eax] = 0;
+  set(esp, 1000);
+  set(ebp, 996);
+  set(eip, 100);
+  set(eax, 0);
   memory[1000] = 0;
 }  // initialize()
 
-void print(Registers *registers, const char *instruction)
+
+void Registers::print(const char *instruction)
 {
-  cout << setw(20) << "eip: " << instruction << "%3d eax: " << registers->get(eip) 
-  << "%3d ebp: " << registers->get(eax) << "%3d esp: "<< registers->get(ebp) 
-  << "%3d\n" << registers->get(esp) << endl;
+  cout  << setw(21) << left << instruction << setw(5) << left << "eip: " 
+      << setw(3) << right << get(eip) << setw(5) << left << " eax: " << setw(3)
+      << right << get(eax) << setw(5) << left << " ebp: " << setw(2) << right 
+      << get(ebp) << setw(5) << left << " esp: "  << left << get(esp) << endl;
 }  // print()
 
 
-int* address(Registers *registers, char *operand, int memory[])
+int* Registers::address(char *operand, int memory[])
 {
   static int value;
   char regNames[4][7] = {"eax", "ebp", "esp", "eip"};
@@ -40,32 +42,34 @@ int* address(Registers *registers, char *operand, int memory[])
   } // if immediate mode
 
   for(regNum = eax; regNum <= eip; regNum++)
-   
-
-
     if(strstr(operand, regNames[regNum]))
       break;
 
-    for (int i = 0; i < sizeof(operand); ++i)
-    {
-      if(operand[i] == '(')
-      {
-        ptr = &operand[i];
-        break;
-      }
-    }
-
-  // ptr = strchr(operand, '(');
+  ptr = strchr(operand, '(');
 
   if(ptr) // some form of indirect addressing
   {
     *ptr = '\0';  // terminate operand string at first '('
     index = atoi(operand);  // will return 0 if no number there!
-    // return  &memory[registers->regs[regNum] + index];
-
-    return &memory[registers.get(regNum) + index]
+    return &memory[get(regNum) + index];
   } // if ptr
   else  // direct addressing
-    return &(registers.get(regNum));
-
+    return &regs[regNum];
 } // address ()
+
+
+
+
+
+int Registers::get(int regnum){
+  if((regnum >= eax) && (regnum <= eip))
+    return regs[regnum]; // check regnum between eax and eip
+  else
+    return 0;
+} // get()
+
+
+void Registers::set(int regnum, int val){
+  if((regnum >= eax) && (regnum <= eip))
+    regs[regnum] = val; // check regnum between eax and eip
+} // set()
