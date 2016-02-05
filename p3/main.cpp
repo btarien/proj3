1cbd040298586ce5239589a633a12da603089d6f
btarien <bmtarien@ucdavis.edu>
2016-02-04 21:16:46 -0800

    Update main.cpp

--- p3/main.cpp
+++ p3/main.cpp
@@ -1,11 +1,10 @@
-//P3 main 
-
 #include "registers.h"
 #include "decoder.h"
 #include "reader.h"
 #include "instruction.h"
 #include <cstdlib>
 #include <cstring>
+
 
 int main(int argc, char* argv[])
 {
@@ -14,17 +13,16 @@ int main(int argc, char* argv[])
   Registers registers;
   Instruction instruction;
   int memory [1001];
-  initialize(&registers, memory);
-  read(&reader, &registers, argv[1]);
-  
-  while(registers.regs[eip] != 0)
+  registers.initialize(memory);
+  reader.read(&registers, argv[1]);
+
+  while(registers.get(eip) != 0)
   {
-    fetch(&reader, &instruction, &registers);
-    parse(&decoder, &instruction, &registers, memory);
-    execute(&decoder, &registers, memory);
-    print(&registers, instruction.info);
+    reader.fetch(&instruction, &registers);
+    decoder.parse(&instruction, &registers, memory);
+    decoder.execute(&registers, memory);
+    registers.print(instruction.getInfo());
   } // while eip not zero
 
   return 0;
 }  // main()
-
